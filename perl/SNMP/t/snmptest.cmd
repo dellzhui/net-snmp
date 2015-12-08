e8d640236ed3fa37a27cb6bf51e79d5114a46b73
root <root@DELL.(none)>
2015-12-08 09:05:08 +0800

    jft

--- perl/SNMP/t/snmptest.cmd
+++
@@ -1,3 +0,0 @@
-SNMPD => /home/work/opensource/net-snmp-5.7.3/out_new/sbin/snmpd
-SNMPTRAPD => /home/work/opensource/net-snmp-5.7.3/out_new/sbin/snmptrapd
-MIBDIR => /home/work/opensource/net-snmp-5.7.3/out_new/share/snmp/mibs
