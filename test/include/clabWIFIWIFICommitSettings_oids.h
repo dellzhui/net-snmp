171afca4d275dd61e6ed14cf3f4f074eca134f02
dellzhui <dellzhui@gmail.com>
2016-03-23 16:43:39 +0800

    GA update 2016-03-23_164339

---
+++ test/include/clabWIFIWIFICommitSettings_oids.h
@@ -0,0 +1,14 @@
+#ifndef CLABWIFIWIFICOMMITSETTINGS_H
+#define CLABWIFIWIFICOMMITSETTINGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define CLABWIFIWIFICOMMITSETTINGSVALUE_OID              1,3,6,1,4,1,4491,2,5,1,1,16,1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
